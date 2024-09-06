937168c25325ab74a7a398e6280069e04a0703f1
Luis De Freites <luis.defreites@intelygenz.com>
2024-09-06 15:26:58 +0200

    feat: Update Navbar component

--- app/ui/navbar.jsx
+++ app/ui/navbar.jsx
@@ -5,54 +5,79 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Link,
-  Button
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Avatar
 } from '@nextui-org/react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const items = [
   {
+    id: 'menus',
     name: 'Menus',
-    href: '/menus'
+    href: '/app/menus'
   },
   {
+    id: 'categories',
     name: 'Categorías',
-    href: '/categories'
+    href: '/app/categories'
   },
   {
+    id: 'items',
     name: 'Artículos',
-    href: '/items'
+    href: '/app/items'
   }
 ]
 
 export default function Navbar({ session }) {
+  const pathname = usePathname()
   return (
-    <NextUINavbar>
+    <NextUINavbar maxWidth="full">
       <NavbarBrand>
         <Link href="/">
-          <p className="font-bold text-inherit">Menu App</p>
+          <p className="font-semibold">Menu App</p>
         </Link>
       </NavbarBrand>
-      <NavbarContent className="sm:flex gap-4" justify="center">
-        {items.map(({ name, href }) => (
-          <NavbarItem>
-            <Link color="foreground" href={href}>
-              {name}
-            </Link>
-          </NavbarItem>
-        ))}
-      </NavbarContent>
+      {session && (
+        <NavbarContent className="sm:flex gap-4" justify="center">
+          {items.map(({ id, name, href }) => (
+            <NavbarItem key={id} isActive={pathname === href}>
+              <Link href={href}>{name}</Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+      )}
       <NavbarContent justify="end">
         {session ? (
-          <NavbarItem>
-            <Button
-              as={Link}
-              color="primary"
-              href="/api/auth/signout"
-              variant="flat"
-            >
-              Sign Out
-            </Button>
-          </NavbarItem>
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="primary"
+                name="Jason Hughes"
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat">
+              <DropdownItem key="profile" isReadOnly className="h-14 gap-2">
+                <p className="font-semibold">Sesión iniciada</p>
+                <p className="font-semibold">{session.user.email}</p>
+              </DropdownItem>
+              <DropdownItem
+                key="logout"
+                color="danger"
+                href="/api/auth/signout"
+              >
+                Cerrar Sesión
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         ) : (
           <NavbarItem>
             <Link href="/auth/login">Login</Link>
